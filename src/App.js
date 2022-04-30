4092785c9932beb1694b0448b3f4a27b0e68219c
cygalternus <swanames@gmail.com>
2022-04-30 00:17:35 -0700

    add randomized quote + disable paste

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { useState, useEffect } from 'react';
 import {animate,stagger} from 'motion';
 let prevShuState =  "shu_d.png";
 let currentShuState = "shu_d.png";
-let currentText = "";
-let fired = false;
+let previousIndex;
 function App() {
 	const [sentText,setSentText] = useState("");
 	const [isSent,setIsSent] = useState(false);
@@ -13,7 +12,9 @@ function App() {
 	const [shuState,setShuState] = useState("shu_d.png");
 	const [enteredText, setEnteredText] = useState(''); 
 	const [quote, setQuote] = useState('ramen is warm cereal'); 
-
+	const [started, setStarted] = useState(false); 
+	const [score, setScore] = useState(0); 
+	const [quoteData,setQuoteData] = useState([]);
 	const [yaminionImg,setYaminionImg] = useState("yaminion.png");
 	const isTyped = (char, index) => {
 		if (enteredText.length-1 >= index){
@@ -26,39 +27,53 @@ function App() {
 		// }
 		return false;
 	}
+	const checkInput = (input) =>{
+		console.log(input)
+		if (input.trimEnd() == quote){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	const onFormSubmit = (e)=>{
 		console.log("send")
 		e.preventDefault();
-		setEnteredText("");
-		setSentText(e.target[0].value);
-		setIsSent(true);
-		animate("#sentText",
-		{ y: -1000, rotate: 900 },
-		{ duration: 1, easing:"linear" });
-		setTimeout(()=>{
-			document.getElementById("explosionSfx").play();
 
-		},150);
-		setQuote("you can sous vide steak in a dishwasher")
-		setTimeout(()=>{
-			setShowExplosion(true);
-			document.getElementById("explosion").src="/images/explosion.gif"
-			document.getElementById("explosionSfx").play();
-			setYaminionImg("yaminion_ko.png");
-			animate("#yaminion",
-				{ x:1000, y: -500, rotate: 900 },
-				{ duration: 1, easing:"linear"  });
-				},250);
-
-		setTimeout(()=>{setShowExplosion(false);document.getElementById("explosion").src="";},900)
-		setTimeout(()=>{document.getElementById("sentText").removeAttribute('style');setYaminionImg("yaminion.png");
-			setIsSent(false);
-			// document.getElementById("yaminion").style.transform.rotate = 0;
-			animate("#yaminion",
-				{ x:[500,0], y: [0,0], rotate: [0,0] },
-				{ duration: 0.5, easing:'ease-out'  });
-				
-		},1500)
+		if (checkInput(e.target[0].value)){
+			setScore(score+1);
+			setEnteredText("");
+			setSentText(e.target[0].value);
+			setIsSent(true);
+			animate("#sentText",
+			{ y: -1000, rotate: 900 },
+			{ duration: 1, easing:"linear" });
+			setTimeout(()=>{
+				document.getElementById("explosionSfx").play();
+	
+			},150);
+			getNextQuote();
+			setTimeout(()=>{
+				setShowExplosion(true);
+				document.getElementById("explosion").src="/images/explosion.gif"
+				document.getElementById("explosionSfx").play();
+				setYaminionImg("yaminion_ko.png");
+				animate("#yaminion",
+					{ x:1000, y: -500, rotate: 900 },
+					{ duration: 1, easing:"linear"  });
+					},250);
+	
+			setTimeout(()=>{setShowExplosion(false);document.getElementById("explosion").src="";},900)
+			setTimeout(()=>{document.getElementById("sentText").removeAttribute('style');setYaminionImg("yaminion.png");
+				setIsSent(false);
+				// document.getElementById("yaminion").style.transform.rotate = 0;
+				animate("#yaminion",
+					{ x:[500,0], y: [0,0], rotate: [0,0] },
+					{ duration: 0.5, easing:'ease-out'  });
+					
+			},1500)
+		}
+		
 	}
 	const createYaminion = () => {
 		const enemyContainer = document.getElementById("enemyContainer");
@@ -108,60 +123,79 @@ function App() {
 			}
 		}
 	}
-	// useEffect(() => {
-	// 	document.addEventListener('keydown', (e)=>{
-	// 		if (!fired){
-	// 			fired = true;
-	// 			console.log(e)
-	// 			console.log(e.keyCode);
-	// 			if ((e.keyCode>= 48 && e.keyCode <= 122) || e.keyCode == 32 || e.keyCode ==  190 || e.keyCode == 222 || e.keyCode == 188 || e.keyCode == 108 ){
-	// 				currentText = currentText + e.key; 
-	// 				setEnteredText(currentText);
-	// 			}
-	// 			else if (e.keyCode == 8 || e.keyCode == 46){
-	// 				console.log(e.keyCode)
-	// 				currentText = currentText.substring(0, currentText.length - 1)
-	// 				setEnteredText(currentText);
+	const getNextQuote = () => {
+		let randomNum =  Math.round(Math.random() * (quoteData.length-1 - 0) + 0);
 
-	// 			}
-
-
-	// 		}; 
-	// 	});
-	// 	document.addEventListener('keyup', (e)=>{fired = false});
-
-	//   }, []);
-	
+		if (previousIndex!=null){
+			while (randomNum == previousIndex){
+				randomNum =  Math.round(Math.random() * (quoteData.length-1 - 0) + 0);
+			}
+		}
+		 previousIndex = randomNum;
+		setQuote(quoteData[randomNum].text);
+	}
+	const getData=()=>{
+		fetch('data/quotes.json'
+		,{
+		  headers : { 
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		   }
+		}
+		)
+		  .then(function(response){
+			console.log(response)
+			return response.json();
+		  })
+		  .then(function(json) {
+			console.log(json);
+			setQuoteData(json);
+			let randomNum =  Math.round(Math.random() * (quoteData.length-1 - 0) + 0);
+			setQuote(json[randomNum].text)
+		  });
+	  };
+	  useEffect(()=>{
+		getData();
+	  },[]);
   return (
     <div className="App">
 		<div className='boundary'>
-		<div className='container'>
-	  <audio id="explosionSfx" src="/sfx/explosion.mp3"></audio>
-	  <div id="scoreContainer">Score</div>
-		  <div id="enemyContainer">
-			  	<img className={showExplosion?"show":"hide"} id="explosion" src="/images/explosion.gif"></img>
-		  		<img id="yaminion" src={`/images/yaminion/${yaminionImg}`}></img>
 
-		  </div>
+			<div className='container'>
+			{
+				!started?
+				<div><button onClick={()=>{setStarted(true)}}>Start</button></div>:
+				<>
+					<audio id="explosionSfx" src="/sfx/explosion.mp3"></audio>
+					<div id="scoreContainer">Score: {score}</div>
+						<div id="enemyContainer">
+								<img className={showExplosion?"show":"hide"} id="explosion" src="/images/explosion.gif"></img>
+								<img id="yaminion" src={`/images/yaminion/${yaminionImg}`}></img>
+			
+						</div>
+			
+						<div className='typingContainer'> 
+								<h2 className={isSent?"show":"hide"} id="sentText">{sentText}</h2>
+			
+							<div className='formContainer'>
+								<form onSubmit={onFormSubmit}>
+									<h2 id="textinput">
+										{[...quote].map((e,index)=>{return <span className={isTyped(e,index)?"typed":""}>{e}</span>})}
+									</h2>
+									<input  onPaste={(e)=>{e.preventDefault();}} maxLength={50} placeholder={quote} autoComplete='off' id="input_phrase" onKeyUp={()=>setShuState("shu_u.png")}  onChange={e => setEnteredText(e.target.value)} onKeyDown={(e)=>keyAnimate(e)} type="text" value={enteredText}></input>
+									<input style={{display:"none"}} disabled={isSent || enteredText.length == 0} type="submit"/>
+			
+								</form>
+							</div>
+			
+							<img src={`/images/shu/${shuState}`}></img>
+						</div>
+				</>
+				
+			}
 
-		  <div className='typingContainer'> 
-		  		<h2 className={isSent?"show":"hide"} id="sentText">{sentText}</h2>
-
-				<div className='formContainer'>
-					<form onSubmit={onFormSubmit}>
-						<h2 id="textinput">
-							{[...quote].map((e,index)=>{return <span className={isTyped(e,index)?"typed":""}>{e}</span>})}
-						</h2>
-						<input maxLength={50} placeholder={quote} autoComplete='off' id="input_phrase" onKeyUp={()=>setShuState("shu_u.png")}  onChange={e => setEnteredText(e.target.value)} onKeyDown={(e)=>keyAnimate(e)} type="text" value={enteredText}></input>
-						<input style={{display:"none"}} disabled={isSent || enteredText.length == 0} type="submit"/>
-
-					</form>
+		  
 				</div>
-
-				<img src={`/images/shu/${shuState}`}></img>
-		  </div>
-
-	  </div>
 		</div>
 
     </div>
